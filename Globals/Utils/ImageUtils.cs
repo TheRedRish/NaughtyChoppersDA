e7968a319e5003ed74a6ed3746150e98bef48b03
EnKop420 <rasmushww@gmail.com>
2023-08-28 23:22:09 +0200

    Made Error Message when image file is over 500 kilobytes in createprofile

--- Globals/Utils/ImageUtils.cs
+++ Globals/Utils/ImageUtils.cs
@@ -4,7 +4,7 @@ namespace NaughtyChoppersDA.Globals.Utils
 {
     public class ImageUtils
     {
-        public static string ConvertProfileImageToSrcImage(Byte[] imageInByteArray)
+        public static string ConvertProfileImageToSrcImage(byte[] imageInByteArray)
         {
             return "data:image/*;base64," + Convert.ToBase64String(imageInByteArray);
         }
